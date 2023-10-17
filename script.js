5fc79551450962f4c1733d4eebd36389ade7c31d
Pritam Sahu <126187108+pritamsahu99@users.noreply.github.com>
2023-10-17 23:30:19 +0530

    added

--- script.js
+++ script.js
@@ -240,6 +240,22 @@
 
 // 18. Write a function that takes an object and returns an array of all the keys in the object.
 
+function objects(obj){
+    let keyArr=[];
+    for (const key in obj) {
+        if (Object.entries.call(obj, key)) {
+            keyArr.push(key);
+        }
+    }
+    return keyArr;
+}
+console.log(objects(
+    {
+     a: 'one', 
+     b: 'two',
+     c:'three'
+    }
+));
 // function keysOfObjects(objects){
 //     let keys = [];
 //     keys.push(...Object.keys(objects));
@@ -272,6 +288,7 @@
 // ));
 
 // 19. Write a function that takes an array of objects and returns an array of all the values of a specified property name.
+
 
 
 
