c0bf1cba7e12d3b8446a5917d9e0df7acce7e845
pritamsahu99 <pritamsahuofficial@gmail.com>
2023-10-17 23:50:12 +0530

    added

--- script.js
+++ script.js
@@ -270,26 +270,30 @@ console.log(objects(
 
 //Or
 
-// function objects(obj){
-//     let keyArr=[];
-//     for (const key in obj) {
-//         if (Object.entries.call(obj, key)) {
-//             keyArr.push(key);
-//         }
-//     }
-//     return keyArr;
-// }
-// console.log(objects(
-//     {
-//      a: 'one', 
-//      b: 'two',
-//      c:'three'
-//     }
-// ));
+function objects(obj){
+    let keyArr=[];
+    for (const key in obj) {
+        if (Object.entries.call(obj, key)) {
+            keyArr.push(key);
+        }
+    }
+    return keyArr;
+}
+console.log(objects(
+    {
+     a: 'one', 
+     b: 'two',
+     c:'three'
+    }
+));
 
 // 19. Write a function that takes an array of objects and returns an array of all the values of a specified property name.
 
 
+<<<<<<< HEAD
+=======
 
 
 
+
+>>>>>>> 5fc79551450962f4c1733d4eebd36389ade7c31d
